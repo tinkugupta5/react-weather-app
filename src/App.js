23eb0c3efd542b0e5aebcda5270dd179bce4cbc5
tinkugupta5 <tinku.gupta4@gmail.com>
2024-05-11 16:54:48 +0530

    work on clean code

--- src/App.js
+++ src/App.js
@@ -4,22 +4,26 @@ import { useEffect } from "react";
 function App() {
   const [city, setCity] = useState("Delhi");
   const [weatherData, setWeatherData] = useState(null);
-  const [error, setError] = useState(null);
-
   const currentDate = new Date();
-
   const months = [
-    "January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
   ];
-
   const month = months[currentDate.getMonth()];
   const day = currentDate.getDate();
   const year = currentDate.getFullYear();
-
   const formattedDate = `${month} ${day}, ${year}`;
-
-  const API_KEY = "bcda10ba323e88e96cb486015a104d1d"; // Replace 'YOUR_API_KEY' with your actual API key from OpenWeatherMap
+  const API_KEY = "bcda10ba323e88e96cb486015a104d1d"; 
 
   const fetchWeatherData = async () => {
     try {
@@ -27,29 +31,16 @@ function App() {
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
       );
       const data = await response.json();
-      console.log(data)
-      // if (response.ok) {
+      console.log(data);
       setWeatherData(data);
-      // setError(null);
-      // } else {
-      //   setError(data.message);
-      //   setWeatherData(null);
-      // }
     } catch (error) {
       console.error("Error fetching weather data:", error);
-      // setError("Error fetching weather data. Please try again later.");
-      // setWeatherData(null);
     }
   };
 
-  useEffect(()=>{
-
-    
-  
-
-  fetchWeatherData();
-
-  },[])
+  useEffect(() => {
+    fetchWeatherData();
+  }, []);
 
   const handleInputChange = (event) => {
     setCity(event.target.value);
@@ -62,12 +53,12 @@ function App() {
 
   const getWeatherIconUrl = (main) => {
     switch (main) {
-      case "Clear":
-        return "/sun.png"; // Path to your sunny weather icon
+      case "Clouds":
+        return "/thunder.png"; // Path to your sunny weather icon
       case "Rain":
-        return "/icons/rainy.png"; // Path to your rainy weather icon
-      case "Snow":
-        return "/icons/snowy.png"; // Path to your snowy weather icon
+        return "/rain_with_cloud.png"; // Path to your rainy weather icon
+      case "Mist":
+        return "/Tornado.png"; // Path to your snowy weather icon
       case "Haze":
         return "/sun.png"; // Path to your haze weather icon
       // Add more cases for other weather conditions as needed
@@ -76,11 +67,8 @@ function App() {
     }
   };
 
-
   return (
     <div className="App">
-      
-
       <div className="container">
         {weatherData && (
           <>
@@ -88,9 +76,17 @@ function App() {
             <div className="weather_data">
               <h2 className="container_city">{weatherData.name}</h2>
               {/* <img className="container_img" src="/thunder.png" width="180px" alt="sss"/> */}
-              <img className="container_img" src={getWeatherIconUrl(weatherData.weather[0].main)} width="180px" alt="Weather Icon" />
+              <img
+                className="container_img"
+                src={getWeatherIconUrl(weatherData.weather[0] .main)}
+                width="180px"
+                alt="Weather Icon"
+              />
               <h2 className="container_degree">{weatherData.main.temp}</h2>
-              <h2 className="country_per">{weatherData.weather[0].main}<span className="degree_icon"></span></h2>
+              <h2 className="country_per">
+                {weatherData.weather[0].main}
+                <span className="degree_icon"></span>
+              </h2>
               <form className="form" onSubmit={handleSubmit}>
                 <input
                   type="text"
@@ -105,8 +101,6 @@ function App() {
             </div>
           </>
         )}
-
-
       </div>
     </div>
   );
