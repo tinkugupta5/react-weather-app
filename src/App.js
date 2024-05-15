8908b727e5cc18cd85254826ec0ab5714ba2ec43
tinkugupta5 <tinku.gupta4@gmail.com>
2024-05-16 00:45:06 +0530

    url chnage

--- src/App.js
+++ src/App.js
@@ -29,7 +29,6 @@ function App() {
     try {
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-        
       );
       const data = await response.json();
       console.log(data);
